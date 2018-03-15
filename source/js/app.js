711f28fdb1c703e84c3140c7a131a078e640c314
pein1625 <pein1625@gmail.com>
2018-03-15 09:52:18 +0700

    dicumin-create-question

--- source/js/app.js
+++ source/js/app.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
-  var search_btn = $('.js-search-btn');
-  var search_box = $('.js-search-box');
+  var $search_btn = $('.js-search-btn');
+  var $search_box = $('.js-search-box');
+  var $question = $('.js-question');
+  var $answer = $('.js-answer');
 
-  search_btn.on('click', function(e) {
+  $search_btn.on('click', function(e) {
     e.stopPropagation();
-    search_box.toggleClass('active');
+    $search_box.toggleClass('active');
   });
 
-  search_box.on('click', function(e) {
+  $search_box.on('click', function(e) {
     e.stopPropagation();
   });
 
   $('html, body').on('click', function() {
-    search_box.removeClass('active');
+    $search_box.removeClass('active');
+  });
+
+  $question.on('click', function(){
+    if ($(this).hasClass('active')) {
+      return;
+    }
+    $question.removeClass('active');
+    $(this).addClass('active');
+    $answer.slideUp(200);
+    $(this).siblings('.js-answer').slideDown(200);
+
   });
 });
