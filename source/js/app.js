77b5739797bfb62cf829ecedbe5de3202f1d4547
pein1625 <pein1625@gmail.com>
2018-03-15 10:56:44 +0700

    dicumin-fixed-container

--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-  var $search_btn = $('.js-search-btn');
-  var $search_box = $('.js-search-box');
+  var $searchBtn = $('.js-search-btn');
+  var $searchBox = $('.js-search-box');
   var $question = $('.js-question');
   var $answer = $('.js-answer');
 
-  $search_btn.on('click', function(e) {
+  $searchBtn.on('click', function(e) {
     e.stopPropagation();
-    $search_box.toggleClass('active');
+    $searchBox.toggleClass('active');
   });
 
-  $search_box.on('click', function(e) {
+  $searchBox.on('click', function(e) {
     e.stopPropagation();
   });
 
   $('html, body').on('click', function() {
-    $search_box.removeClass('active');
+    $searchBox.removeClass('active');
   });
 
   $question.on('click', function(){
@@ -25,6 +25,5 @@ $(document).ready(function() {
     $(this).addClass('active');
     $answer.slideUp(200);
     $(this).siblings('.js-answer').slideDown(200);
-
   });
 });
